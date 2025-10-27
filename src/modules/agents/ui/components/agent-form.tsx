d3c40cfc6c0bd76bc4a0f7ef0780140405a69a21
Vishal Maurya <mauryavishal767@gmail.com>
2025-10-27 21:35:02 +0530

    agent update delete

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -29,6 +29,24 @@ export const AgentForm = ({onSuccess, onCancel, initialValues}: AgentFormProps) 
                     trpc.agents.getMany.queryOptions({})
                 );
 
+                // TODO:invalidate free tier usage
+                onSuccess?.();
+            },
+            onError: (error) =>{
+                toast.error(error.message);
+
+                // TODO: check if error code is "Forbidden",redirect to /upgrade
+            },
+        })
+    );
+
+    const updateAgent = useMutation(
+        trpc.agents.update.mutationOptions({
+            onSuccess: async () => {
+                await queryClient.invalidateQueries(
+                    trpc.agents.getMany.queryOptions({})
+                );
+
                 if(initialValues?.id) {
                     await queryClient.invalidateQueries(
                         trpc.agents.getOne.queryOptions({ id: initialValues.id })
@@ -53,12 +71,11 @@ export const AgentForm = ({onSuccess, onCancel, initialValues}: AgentFormProps) 
     })
 
     const isEdit = !!initialValues?.id;
-    // const isPending = createAgent.isPending || updateAgent.isPending;
-    const isPending = createAgent.isPending;
+    const isPending = createAgent.isPending || updateAgent.isPending;
 
     const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
         if(isEdit) {
-            console.log("TODO: Update agent");
+            updateAgent.mutate({...values, id:initialValues.id})
         } else {
             createAgent.mutate(values);
         }

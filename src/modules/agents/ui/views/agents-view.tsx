80cc7c7d605374d5b74590ffe9dfab6822d704a0
Vishal Maurya <mauryavishal767@gmail.com>
2025-10-28 03:19:49 +0530

    meetings data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,14 +3,13 @@
 import LoadingState from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import EmptyState from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
-import { NextDataPathnameNormalizer } from "next/dist/server/normalizers/request/next-data";
 import { DataPagination } from "../components/data-pagination";
 import { useRouter } from "next/navigation";
 import ErrorState from "@/components/error-state";
+import { DataTable } from "@/components/data-table";
 
 export const AgentsView = () => {
     const router = useRouter();
@@ -24,7 +23,7 @@ export const AgentsView = () => {
    return (
 
        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-           <DataTable 
+           <DataTable
                 data={data.items} 
                 columns={columns} 
                 onRowClick={(row) => router.push(`/agents/${row.id}`)}

cb7bd598c2a02123c05674b770d275114cda54ea
Vishal Maurya <mauryavishal767@gmail.com>
2025-10-27 15:14:54 +0530

    agents form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,19 +1,34 @@
 import LoadingState from "@/components/loading-state";
+import { auth } from "@/lib/auth";
+import { AgentsListHeaders } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
-const Page = () => {
+const Page = async () => {
+    const session = await auth.api.getSession({
+            headers: await headers(),
+        })
+    
+        if(!session){
+            redirect('/sign-in');
+        }
+
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions())
 
     return(
+    <>
+        <AgentsListHeaders />
         <HydrationBoundary state={dehydrate(queryClient)}>
             <Suspense fallback={<AgentsViewLoading />}>
                 <AgentsView/>
             </Suspense>
         </HydrationBoundary>
+    </>
     );
 }
 
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -56,7 +56,7 @@ export const agents = pgTable("agents",{
     userId: text("user_id")
         .notNull()
         .references(()=> user.id, { onDelete: "cascade" }),
-    instruction: text("instruction").notNull(),
+    instructions: text("instructions").notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
 })

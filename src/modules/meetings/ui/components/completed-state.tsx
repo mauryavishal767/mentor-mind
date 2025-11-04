03b0e25550155af1151c00a9da8bc595b095d71e
Vishal Maurya <mauryavishal767@gmail.com>
2025-11-04 10:21:08 +0530

    transcript & chat

--- src/modules/meetings/ui/components/completed-state.tsx
+++ src/modules/meetings/ui/components/completed-state.tsx
@@ -8,10 +8,13 @@ import { format } from "date-fns";
 import { Badge } from "@/components/ui/badge";
 import { formatDuration } from "@/lib/utils";
 import Markdown from "react-markdown";
+import { Transcript } from "./transcript";
+import { ChatProvider } from "./chat-provider";
 
 interface Props {
 data: MeetingGetOne;
 }
+
 export const CompletedState= ({ data }: Props) => {
     return (
         <div className="flex flex-col gap-y-4">
@@ -51,6 +54,12 @@ export const CompletedState= ({ data }: Props) => {
                         <ScrollBar orientation="horizontal" />
                     </ScrollArea>
                 </div>
+                <TabsContent value="chat">
+                    <ChatProvider meetingId={data.id} meetingName={data.name} />
+                </TabsContent>
+                <TabsContent value="transcript">
+                    <Transcript meetingId={data.id}/>
+                </TabsContent>
                 <TabsContent value="recording">
                     <div className="bg-white rounded-lg border px-4 py-5">
                         <video 
